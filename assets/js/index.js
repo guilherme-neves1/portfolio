23031f3fc1ac6dcfe05ce758d02fae55ce7635e0
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-02-13 09:37:03 -0300

    add scroll reveal in projects

--- assets/js/index.js
+++ assets/js/index.js
@@ -217,3 +217,22 @@ if (window.innerWidth < 700) {
   });
 }
 // ==================== END SKILLS SWIPER ====================
+
+// ==================== SCROLL REVEAL ====================
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    const windowheight = window.innerHeight;
+    const revealtop = reveals[i].getBoundingClientRect().top;
+    const revealpoint = 20;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
